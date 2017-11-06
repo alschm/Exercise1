f795049dd31844a0c6048b8c8d83ab29594e9e29
Alexander Schmidle <alex.schmidle@tum.de>
2017-11-06 09:54:56 +0100

    First Solution exercise 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
-	}
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter your name: ");
+		try {
+			String input = br.readLine();
+			HelloUser user = new HelloUser(input);
+			user.greetUser();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
